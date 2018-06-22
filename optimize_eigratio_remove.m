3626067be9b3f3a6090150b087a273b98758ecf1
y-z-zhang <zhangyuanzhao45@gmail.com>
2018-06-22 15:10:45 -0500

    Update optimize_eigratio_remove.m

--- optimize_eigratio_remove.m
+++ optimize_eigratio_remove.m
@@ -1,5 +1,5 @@
 %% A simulated annealing algorithm to improve the synchronizability 
-%% of networks through minimal link rewiring 
+%% of networks through minimal link removal 
 %%                      by Yuanzhao Zhang (yuanzhao@u.northwestern.edu)
 
 %% adjacency matrix of the original network
@@ -19,7 +19,7 @@ r_target = r_original/2 + .5
 
 %% number of independent simulated annealings to be performed
 N = 5*1e4;
-%% m records the number of links rewired before the target eigen-ratio is reached at each run
+%% m records the number of links removed before the target eigen-ratio is reached at each run
 m = zeros(1,N);
 % inverse temperature used in simulated annealings (different for each run)
 Beta = linspace(50,500,N);
@@ -33,29 +33,26 @@ for i = 1:N
     %% start of simulated annealing
     while r_new > r_target
         aij = adj;
-        %% at each iteration between 1 to 4 links will be randomly rewired
-        num_rewire = randi(4);
+        %% at each iteration between 1 to 4 links will be randomly removed
+        num_rem = randi(4);
         idx = find(adj == 1);
-        del_link = idx(randperm(size(idx,1),num_rewire));
+        del_link = idx(randperm(size(idx,1),num_rem));
         aij(del_link) = 0;
-        %ind = find(adj == 0);
-        %add_link = ind(randperm(size(ind,1),num_rewire));
-        %aij(add_link) = 1;
-        %% find the eigen-ratio of the rewired network
+        %% find the eigen-ratio of the new network
         rowsum = sum(aij,2);
         lap = diag(rowsum) - aij;
         %% next line requires newer version of MATLAB
         eigv = sort(eig(lap),'ComparisonMethod','real');
         r_new = real(eigv(end))/real(eigv(2));
-        %% energy is negative if the eigen-ratio is improved by rewiring
+        %% energy is negative if the eigen-ratio is improved by removal
         energy = r_new/min(r_original,r_current)-1;
 
         %% Metropolis criterion
         if rand < exp(-beta*energy)
             adj = aij;
             r_current = r_new;
-            m(i) = m(i)+num_rewire;
-            %% if more than M rewirings have been performed, abort this run
+            m(i) = m(i)+num_rem;
+            %% if more than M removals have been performed, abort this run
             if m(i) > M
                 break
             end
@@ -64,14 +61,14 @@ for i = 1:N
     %% if the target eigen-ratio is reached
     if r_current <= r_target
         m(i) = size(find(vtg-adj==1),1);
-        %% if this successful run needs the smallest number of rewirings so far
+        %% if this successful run needs the smallest number of removals so far
         if m(i) == min(m(1:i))
-            %% print the number of links rewired
+            %% print the number of links removed
             minimal_num = m(i)
-            %% print the percentage of links rewired
-            rewire_ratio = minimal_num/M
+            %% print the percentage of links removed
+            rem_ratio = minimal_num/M
             %% write the optimized newtork to file
-            %dlmwrite('Network1_opt.txt',adj)
+            dlmwrite('Network1_opt.txt',adj)
         end
     end
 end
